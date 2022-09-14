31be8e144e8f061c15da45e5c81dd50699b43e52
succhi111 <suchendramishra396@gmail.com>
2022-09-14 14:03:49 +0530

    Refactored:Created a simple linkedlist

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node<T> node = head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = newNode;
+        }
+    }
+
+    public void print() {
+        Node<T> node = head;
+        while (node.next != null) {
+            System.out.print(node.data + " -> ");
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+}
