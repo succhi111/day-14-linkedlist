3fd94a53f0d2a2cc86580618839691e602addd82
succhi111 <suchendramishra396@gmail.com>
2022-09-14 14:18:20 +0530

    added:adding 56 and 30 to 70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -17,6 +17,15 @@ public class LinkedList<T> {
         }
     }
 
+    public void addAtStart(T data) {
+
+        Node<T> newNode = new Node<>();
+        newNode.data = data;
+        newNode.next = head;
+        head = newNode;
+
+    }
+
     public void print() {
         Node<T> node = head;
         while (node.next != null) {
